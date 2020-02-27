d38662861e3fbf8061cc7cbe83b2a481ac0e15ff
ridhijain21 <61418794+ridhijain21@users.noreply.github.com>
2020-02-27 15:15:36 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -14,7 +14,7 @@ def hello():
 def reco():
     conn = psycopg2.connect(database="postgres", user="postgres", password="123post", host="127.0.0.1", port="5432")
     cur = conn.cursor()
-    cur.execute('''select a.language_code from (select language_code, count(*) from books ) a order by a.count desc''')
+    cur.execute('''select a.language_code from (select language_code, count(*) from books group by language_code ) a order by a.count desc''')
     a = []
     row = cur.fetchone()
     while row is not None:
@@ -32,7 +32,7 @@ def display():
         #print(tag_name, author_name, rating, lang)
         conn = psycopg2.connect(database="postgres", user="postgres", password="123post", host="127.0.0.1", port="5432")
         cur = conn.cursor()
-        cur.execute('''select a.language_code from (select language_code, count(*) from books ) a order by a.count desc''')
+        cur.execute('''select a.language_code from (select language_code, count(*) from books group by language_code) a order by a.count desc''')
         a = []
         row = cur.fetchone()
         while row is not None:
@@ -56,19 +56,18 @@ def display():
         where original_publication_year != '') a
         where a.num between {0} and {1} '''.format(publication[1:5], publication[6:10]))
 
-        cur.execute('''create view result as select distinct fr.title, fr.average_rating, fy.num, b.ratings_count
+        cur.execute('''create view result as select distinct fr.title, fr.average_rating, fy.num, b.ratings_count, b.authors
         from filter_ratings fr, filter_tags ft, filter_year fy, books b
         where fr.id = ft.id and ft.id = fy.id and fy.id = b.id and b.language_code = '{0}' '''.format(lang))
 
-        cur.execute('''select title from result''')
+        cur.execute('''select title, average_rating, authors from result''')
         item = []
         row = cur.fetchone()
+        #print(row, row[0], row[1])
         while row is not None:
-            item.append(row[0])
+            item.append(row)
             row = cur.fetchone()
-        cur.execute('''drop view filter_tags''')
-        cur.execute('''drop view filter_ratings''')
-        cur.execute('''drop view filter_year''')
+        #print(item)
         if item == []:
             return render_template('display.html', message = "[No results]")
         else:
@@ -76,26 +75,25 @@ def display():
 
 @app.route('/display', methods=['GET', 'POST'])
 def book_info():
-    if request.method == 'POST':
-        sorting_parameter = request.args.get("sorting_parameter")
-        conn = psycopg2.connect(database="postgres", user="postgres", password="123post", host="127.0.0.1", port="5432")
-        cur = conn.cursor()
-        if sorting_parameter == 'ratings':
-            cur.execute('''Select title from result order by average_rating desc''')
+    sort = request.args.get("sorting_parameter")
+    print(sort)
+    conn = psycopg2.connect(database="postgres", user="postgres", password="123post", host="127.0.0.1", port="5432")
+    cur = conn.cursor()
+    if sort == 'ratings':
+        cur.execute('''Select title, average_rating, authors from result order by average_rating desc''')
 
-        if sorting_parameter == 'rating_count':
-            cur.execute('''Select title from result order by ratings_count desc''')
+    if sort == 'rating_count':
+        cur.execute('''Select title, average_rating, authors from result order by ratings_count desc''')
 
-        if sorting_parameter == 'latest_year':
-            cur.execute('''Select title from result order by num desc''')
+    if sort == 'latest_year':
+        cur.execute('''Select title, average_rating, authors from result order by num desc''')
 	    
-        item = []
+    item = []
+    row = cur.fetchone()
+    while row is not None:
+        item.append(row)
         row = cur.fetchone()
-        while row is not None:
-            item.append(row[0])
-            row = cur.fetchone()
-
-        return render_template('display.html', message = item)
+    return render_template('display.html', message = item)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -115,7 +113,25 @@ from ratings
 union all (select distinct user_id from to_read)) a) b where b.user_id = '{0}' '''.format(user))
     item = cur.fetchall()
     if item:
-
+        #leaderboard
+        cur.execute('''select ROW_NUMBER() OVER(ORDER BY (SELECT 1)) as sno, user_id, count as score
+        from (select user_id, count(*)
+        from ratings 
+        group by user_id
+        order by count desc) a ''')
+        leader_result = []
+        row=cur.fetchone()
+        while row is not None:
+            leader_result.append(row)
+            row = cur.fetchone()
+        
+        cur.execute('''select b.title, a.count
+from (select book_id, count(*)
+from to_read
+group by book_id) a, books b
+where a.book_id = b.id
+order by count desc''')
+        
         return render_template('user_page.html')
     else:
         return render_template('login.html', message = "USER DOES NOT EXISTS")
